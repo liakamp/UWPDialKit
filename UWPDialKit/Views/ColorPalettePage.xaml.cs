6581b370c3e1a679205722c83e29d02150048c66
liakamp <liakamp@outlook.com>
2017-04-12 22:16:49 +0300

    color saturation selection in color pallete

--- UWPDialKit/Views/ColorPalettePage.xaml.cs
+++ UWPDialKit/Views/ColorPalettePage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using UWPDialKit.Helpers;
 using Windows.Foundation;
 using Windows.UI;
@@ -12,13 +11,19 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
 namespace UWPDialKit
-{   
+{
     public sealed partial class ColorPalettePage : Page
     {
+        public ColorSelectionMode SelectionMode;
+        float mainColorAngle;
+        float activeSaturationValue;
+        Grid PaletteRing;
+        Grid SaturationPaletteRing;
         private RadialController Controller;
         Storyboard storyboard;
         bool isRotating;
         double currentAngle;
+
         public ColorPalettePage()
         {
             InitializeComponent();
@@ -41,61 +46,122 @@ namespace UWPDialKit
 
         private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
         {
-            Grid PaletteRing = new Grid();
+            SelectionMode = ColorSelectionMode.Value;
+            PaletteRing = new Grid();
             PaletteRing.Width = 300;
             PaletteRing.Height = 300;
-            Color current = Utilities.ConvertHSV2RGB(0,1,1);
+            Color current = Utilities.ConvertHSV2RGB(0, 1, 1);
             for (int i = 0; i < 360; i++)
             {
                 RotateTransform rt = new RotateTransform() { Angle = i };
                 LinearGradientBrush colorBrush = new LinearGradientBrush();
                 colorBrush.GradientStops.Add(new GradientStop() { Color = current });
-                current = Utilities.ConvertHSV2RGB(i,1,1);
+                current = Utilities.ConvertHSV2RGB(i, 1, 1);
                 colorBrush.GradientStops.Add(new GradientStop() { Color = current, Offset = 1 });
-                Path line = new Path()
-                {
-                    Data = new LineGeometry() { StartPoint = new Point(280, 150), EndPoint = new Point(300, 150) },
-                    StrokeThickness = 3,
-                    RenderTransformOrigin = new Point(0.5, 0.5),
-                    RenderTransform = rt,
-                    Stroke = colorBrush
-                };
+                Path line = DrawColorLine(rt, colorBrush);
                 PaletteRing.Children.Add(line);
             }
             PaletteRing.HorizontalAlignment = HorizontalAlignment.Center;
             PaletteRing.VerticalAlignment = VerticalAlignment.Center;
             LayoutRoot.Children.Add(PaletteRing);
+            SaturationPaletteRing = new Grid()
+            {
+                Width = 300,
+                Height = 300,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            LayoutRoot.Children.Add(SaturationPaletteRing);
+        }
+
+        private static Path DrawColorLine(RotateTransform rt, LinearGradientBrush colorBrush)
+        {
+            return new Path()
+            {
+                Data = new LineGeometry() { StartPoint = new Point(280, 150), EndPoint = new Point(300, 150) },
+                StrokeThickness = 3,
+                RenderTransformOrigin = new Point(0.5, 0.5),
+                RenderTransform = rt,
+                Stroke = colorBrush
+            };
         }
 
         private void InitializeController()
         {
             Controller = RadialController.CreateForCurrentView();
-            Controller.RotationResolutionInDegrees = 1;            
+            Controller.RotationResolutionInDegrees = 1;
             Controller.RotationChanged += Controller_RotationChanged;
-            Controller.ButtonClicked += Controller_ButtonClicked;         
+            Controller.ButtonClicked += Controller_ButtonClicked;
         }
 
         private void CreateMenuItem()
         {
             RadialControllerMenuItem radialControllerItem = RadialControllerMenuItem.CreateFromKnownIcon("Palette", RadialControllerMenuKnownIcon.InkColor);
             Controller.Menu.Items.Add(radialControllerItem);
-        }            
+        }
 
         private void Controller_ButtonClicked(RadialController sender, RadialControllerButtonClickedEventArgs args)
         {
-            SelectedColor.Background = new SolidColorBrush(Utilities.ConvertHSV2RGB((float)Utilities.ClampAngle(currentAngle),1,1));
+            if (SelectionMode == ColorSelectionMode.Value)
+            {
+                mainColorAngle = (float)Utilities.ClampAngle(currentAngle);
+                SelectedColorBorder.Background = new SolidColorBrush(Utilities.ConvertHSV2RGB(mainColorAngle, 1, 1));
+                SelectionMode = ColorSelectionMode.Saturation;
+            }
+            else
+            {
+                SelectedColorBorder.Background = new SolidColorBrush(Utilities.ConvertHSV2RGB(mainColorAngle, 1 - Math.Abs(activeSaturationValue), 1));
+                SelectionMode = ColorSelectionMode.Value;
+                activeSaturationValue = 0;
+            }
+            FillSelectionRing();
+
+        }
+
+        private void FillSelectionRing()
+        {
+            if (SelectionMode == ColorSelectionMode.Value)
+            {
+                PaletteRing.Visibility = Visibility.Visible;
+                SaturationPaletteRing.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                PaletteRing.Visibility = Visibility.Collapsed;
+                FillSaturationRing();
+                SaturationPaletteRing.Visibility = Visibility.Visible;
+
+            }
+        }
+
+        private void FillSaturationRing()
+        {
+            SaturationPaletteRing.Children.Clear();
+            Color current = Utilities.ConvertHSV2RGB(mainColorAngle, 0, 1);
+            double rotationAngle = Utilities.ClampAngle(mainColorAngle - 100);
+            for (int i = 0; i < 100; i++)
+            {
+                RotateTransform rt = new RotateTransform() { Angle = Utilities.ClampAngle(rotationAngle) };
+                rotationAngle++;
+                LinearGradientBrush colorBrush = new LinearGradientBrush();
+                colorBrush.GradientStops.Add(new GradientStop() { Color = current });
+                current = Utilities.ConvertHSV2RGB(mainColorAngle, i * 0.01f, 1);
+                colorBrush.GradientStops.Add(new GradientStop() { Color = current, Offset = 1 });
+                Path line = DrawColorLine(rt, colorBrush);
+                SaturationPaletteRing.Children.Add(line);
+            }
         }
 
         private void Controller_RotationChanged(RadialController sender, RadialControllerRotationChangedEventArgs args)
-        {           
+        {
             Rotate(args.RotationDeltaInDegrees);
-        }       
+        }
 
         private void Storyboard_Completed(object sender, object e)
         {
             isRotating = false;
             CompleteRotation();
-        }       
+        }
 
         private void Rotate(double angle = 1)
         {
@@ -105,6 +171,18 @@ namespace UWPDialKit
             }
 
             DoubleAnimation rayAnimation = new DoubleAnimation();
+
+            if (SelectionMode == ColorSelectionMode.Saturation)
+            {
+                double destinationAngle = Utilities.ClampAngle(currentAngle + angle);
+
+                if (!IsInSaturationBounds(destinationAngle))
+                {
+                    return;
+                }
+                activeSaturationValue += (float)angle / 100;
+            }
+
             currentAngle += angle;
             rayAnimation.To = currentAngle;
             rayAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
@@ -123,9 +201,32 @@ namespace UWPDialKit
 
             storyboard.Begin();
             isRotating = true;
-            Color activeColor = Utilities.ConvertHSV2RGB((float)Utilities.ClampAngle(currentAngle),1,1);
-            ColorEllipse.Fill = CurrentColor.Background = 
-                new SolidColorBrush(activeColor);
+
+            Color activeColor;
+            if (SelectionMode == ColorSelectionMode.Value)
+            {
+                activeColor = Utilities.ConvertHSV2RGB((float)Utilities.ClampAngle(currentAngle), 1, 1);
+            }
+            else
+            {
+                activeColor = Utilities.ConvertHSV2RGB(mainColorAngle, 1 - Math.Abs(activeSaturationValue), 1);
+            }
+
+            ColorEllipse.Fill = CurrentColorBorder.Background = new SolidColorBrush(activeColor);
+        }
+
+        private bool IsInSaturationBounds(double destinationAngle)
+        {
+            if (mainColorAngle >= 100)
+            {
+                return (destinationAngle > mainColorAngle ||
+                    destinationAngle < Utilities.ClampAngle(mainColorAngle - 100)) ? false : true;
+            }
+            else
+            {
+                return (destinationAngle < mainColorAngle ||
+                    (destinationAngle > mainColorAngle && destinationAngle > Utilities.ClampAngle(mainColorAngle - 100))) ? true : false;
+            }
         }
 
         private void CompleteRotation()
@@ -136,9 +237,9 @@ namespace UWPDialKit
             ColorRay.RenderTransform = rotateTransform;
             ColorEllipse.RenderTransform = rotateTransform;
             storyboard.Children.Clear();
-        }        
+        }
 
-        
-                      
+        public enum ColorSelectionMode { Value, Saturation }
+
     }
 }
